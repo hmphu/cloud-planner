8d6f60ce3a17db834fb97cf4f49ef5e55c64af81
kjh <kjh@ncsoft.com>
2017-02-01 21:04:25 +0900

    fix cost function

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -14,33 +14,27 @@ class Provider < ApplicationRecord
   end
 
   def self.cost(p, r, m, os, opts = {})
+    opts.symbolize_keys!
+    opts.transform_values! {|x| x.to_s}
+
     provider = Provider.find_by(name: p)
     region = provider.regions.find_by(name: r)
     machine_type = provider.machine_types.find_by(name: m)
 
-    pre_installed_sw  = opts['pre_installed_sw']  || 'na'
-    contract_type     = :on_demand
-    tenancy           = opts['tenancy']   || :shared
-    unit              = opts['unit']      || :hourly
-    count             = opts['count']     || 1
+    pre_installed_sw  = opts[:pre_installed_sw]  || 'na'
+    tenancy           = opts[:tenancy]   || 'shared'
+    unit              = opts[:unit]      || 'hourly'
+    count             = (opts[:count]     || 1).to_i
+    offering_class    = nil
+    contract_type = opts[:contract] || 'on_demand'
+    prepay_type       = nil
 
-
-    ap opts
-
-    if(['1y', '3y'].include? opts['contract'])
-      contract_type = 'ri_' + opts['contract'] + '_' + opts['upfront']
+    if(contract_type != 'on_demand')
+      offering_class    = opts[:offering_class] || :standard
+      prepay_type = 'prepay_' + (opts[:upfront] || 'all').to_s
     end
 
-    if(contract_type == :on_demand)
-      offering_class = nil
-    else
-      offering_class    = opts['offering_class'] || :standard
-    end
-
-    puts 'xxx contract_type'
-    puts contract_type
-
-    if opts['upfront'] == 'all'
+    if opts[:upfront] && (opts[:upfront].to_sym == :all)
       cost = 0
     else
       instances = provider.instance_types.where(
@@ -50,6 +44,7 @@ class Provider < ApplicationRecord
 
         pre_installed_sw: pre_installed_sw,
         contract_type: contract_type,
+        prepay_type: prepay_type,
         tenancy: tenancy,
         unit: unit,
         offering_class: offering_class,
@@ -57,33 +52,37 @@ class Provider < ApplicationRecord
 
       if instances.count > 1
         instances.each {|i| ap i}
+        return "ERROR: Too many instance types"
+      end
+
+      if instances.count == 0
+        return "ERROR: No such instance type"
       end
 
 
       inst = instances.first
       cost = inst.price
-      ap inst
     end
 
 
-    if ['ri_1y_partial', 'ri_1y_all', 'ri_3y_partial', 'ri_3y_all'].include? contract_type
+    if ['ri_1y', 'ri_3y'].include?(contract_type) && opts[:upfront] != 'no'
       inst2 = provider.instance_types.where(
         region_id: region.id,
         machine_type_id: machine_type.id,
         os_type: os.downcase,
         pre_installed_sw: pre_installed_sw,
         contract_type: contract_type,
+        prepay_type: prepay_type,
         tenancy: tenancy,
         offering_class: offering_class,
 
         unit: :upfront,
       ).first
 
-      ap inst2
       prepay = inst2.price
 
-      period = 1 if ['ri_1y_partial', 'ri_1y_all'].include? contract_type
-      period = 3 if ['ri_3y_partial', 'ri_3y_all'].include? contract_type
+      period = 1 if contract_type == 'ri_1y'
+      period = 3 if contract_type == 'ri_3y'
 
       upfront_hourly = prepay / (period * 365 * 24)
       cost = cost + upfront_hourly
@@ -94,7 +93,7 @@ class Provider < ApplicationRecord
 
     desc = [p, r, m, os, pre_installed_sw, contract_type, tenancy, unit, count.to_s].join(', ')
 
-    return desc, cost
+    return desc, cost.round(3)
   end
 
 end
